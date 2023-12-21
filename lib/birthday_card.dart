abf379cf811cbeba9ba66e95f727422455cc5a38
Dalso <flskwhdhk@gmail.com>
2023-12-21 23:02:35 +0900

    뒤에 이미지 배경 씌우기

--- lib/birthday_card.dart
+++ lib/birthday_card.dart
@@ -5,37 +5,47 @@ class BirthdayCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Happy',
-              style: TextStyle(fontSize: 90),
-            ),
-            Text(
-              'Birthday',
-              style: TextStyle(fontSize: 90),
-            ),
-            Text(
-              'Sam!',
-              style: TextStyle(fontSize: 90),
-            ),
-            SizedBox(
-              width: double.maxFinite,
-              child: Padding(
-                padding: EdgeInsets.only(right: 32),
-                child: Text(
-                  textAlign: TextAlign.right,
-                  'From DongWan',
-                  style: TextStyle(fontSize: 25,color: Colors.black45),
+    return Scaffold(
+      body: Stack(children: [
+        SizedBox(
+          child: Image.network(
+              'https://st3.depositphotos.com/11630730/16840/v/450/depositphotos_168404358-stock-illustration-winter-landscape-with-snow.jpg'
+              , fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+          ),
+        ),
+        const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Happy',
+                style: TextStyle(fontSize: 90),
+              ),
+              Text(
+                'Birthday',
+                style: TextStyle(fontSize: 90),
+              ),
+              Text(
+                'Sam!',
+                style: TextStyle(fontSize: 90),
+              ),
+              SizedBox(
+                width: double.maxFinite,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 32),
+                  child: Text(
+                    textAlign: TextAlign.right,
+                    'From DongWan',
+                    style: TextStyle(fontSize: 25, color: Colors.black45),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
