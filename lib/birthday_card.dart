1491f261e39587de8b0b1a31a7fb4b9079290971
Dalso <flskwhdhk@gmail.com>
2023-12-22 10:02:58 +0900

    Lotti 라이브러리를 활용하여 크리스마스 느낌나게 해보기

--- lib/birthday_card.dart
+++ lib/birthday_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class BirthdayCard extends StatelessWidget {
   const BirthdayCard({super.key});
@@ -9,41 +10,39 @@ class BirthdayCard extends StatelessWidget {
       body: Stack(children: [
         SizedBox(
           child: Image.network(
-              'https://st3.depositphotos.com/11630730/16840/v/450/depositphotos_168404358-stock-illustration-winter-landscape-with-snow.jpg'
-              , fit: BoxFit.cover,
-              width: double.infinity,
-              height: double.infinity,
+            'https://st3.depositphotos.com/11630730/16840/v/450/depositphotos_168404358-stock-illustration-winter-landscape-with-snow.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
           ),
         ),
-        const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Happy',
-                style: TextStyle(fontSize: 90),
-              ),
-              Text(
-                'Birthday',
-                style: TextStyle(fontSize: 90),
-              ),
-              Text(
-                'Sam!',
-                style: TextStyle(fontSize: 90),
-              ),
-              SizedBox(
-                width: double.maxFinite,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 32),
+        const Align(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: EdgeInsets.only(top: 40),
+            child: Column(
+              children: [
+                Text(
+                  'Happy Birthday Sam!',
+                  style: TextStyle(fontSize: 36),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8),
                   child: Text(
                     textAlign: TextAlign.right,
                     'From DongWan',
-                    style: TextStyle(fontSize: 25, color: Colors.black45),
+                    style: TextStyle(fontSize: 22, color: Colors.black),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+        ),
+        Center(
+          child: SizedBox(
+              height: 300,
+              width: 300,
+              child: Lottie.asset('assets/Animation - 1703205601613.json')),
         ),
       ]),
     );
